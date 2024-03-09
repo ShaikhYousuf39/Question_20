1acee2c52c68120b847e4c91eac8c77ea53b758c
YousufSH <160315514+ShaikhYousuf39@users.noreply.github.com>
2024-03-10 01:15:55 +0500

    Add files via upload

---
+++ new.js
@@ -0,0 +1,6 @@
+"use strict";
+let animals = ["Tiger", "Goat", "Cat", "Camel", 'Cow'];
+console.log("List of animals:");
+for (let top of animals) {
+    console.log(top);
+}
---
+++ new.ts
@@ -0,0 +1,6 @@
+let animals : string[] = ["Tiger","Goat","Cat","Camel",'Cow']
+
+console.log("List of animals:")
+for(let top of animals){
+    console.log(top)
+}
